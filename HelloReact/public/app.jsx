ee8334611dfc1e936ad0300519df3f534b98b5da
Zeck <tools@3csonline.com>
2016-09-04 02:23:01 +0000

    finished the first section HelloReact and solved the challenge without help.

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,10 +1,4 @@
-var Greeter = React.createClass({
-  getDefaultProps: function() {
-    return {
-      name: 'React',
-      message: 'This is from a component'
-    };
-  },
+var GreeterMessage = React.createClass({
   render: function() {
     var name = this.props.name;
     var message = this.props.message;
@@ -18,9 +12,78 @@ var Greeter = React.createClass({
   }
 });
 
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+    
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+    
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+    
+    if (message.length > 0) {
+      this.refs.message.value = "";
+      updates.message = message;
+    }
+    
+    this.props.onNewName(updates);
+  },
+  
+  render: function() {
+    return (
+      <form onSubmit = {this.onFormSubmit}>
+        <input type="text" ref="name" placeholder="Name"/>
+        <br/>
+        <textarea ref="message" placeholder="Enter message"/>
+        <br/>
+        <button>Update</button>
+      </form>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  
+  getInitialState: function() {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    };
+  },
+  
+  handleNewName: function(updates) {
+    // update the state.name
+    this.setState(updates);
+    //alert(name);
+  },
+  
+  render: function() {
+    var name = this.state.name;
+    var message = this.state.message;
+    
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        
+        <GreeterForm onNewName={this.handleNewName}/>
+      </div>
+    );
+  }
+});
+
 var firstName = 'Cliff';
 
 ReactDOM.render(
-  <Greeter name={firstName} message="Just testing my prop"/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
